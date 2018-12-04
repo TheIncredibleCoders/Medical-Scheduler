b0736301b9b4ce45a32843f2767b18ebbc30c062
karan kohli <kkohli@cpp.edu>
2018-12-03 18:32:38 -0800

    updated ui

---
+++ Java-EE---Shared-CalendarV3_SpringMVC-master/source/production/java/com/DAO/EventDao.java
@@ -0,0 +1,22 @@
+package com.DAO;
+
+import com.Calendar.Event;
+
+import java.util.Date;
+import java.util.List;
+
+public interface EventDao {
+    void insertEvent(Event event);
+    void createEventTable();
+    void dropEventTable();
+    boolean eventsExists(String username);
+    int countEvents();
+    boolean eventsExists();
+    boolean hasEvent(String eventname, String username, String eventAurthor, String patientlastname, String patientdob,
+    		String patientfirstname, String patinetins, String patinetphone, String email, String SSN);
+    List<Event> selectRecentEvent(String username);
+    List<Event> selectAllEvent(String username);
+    List<Event> selectAllEvents();
+    Event getEventById(int patientid);
+	
+}
